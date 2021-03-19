7100bdd0fd1e61494b1e375e2622a78dc434f484
Gustavo <luisgustavolg1033@gmail.com>
2021-03-18 21:50:23 -0300

    created category field for form ad

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/keys/secret.dart';
 import 'package:xlo_mobx/screens/base/base_screen.dart';
+import 'package:xlo_mobx/screens/category/category_screen.dart';
+import 'package:xlo_mobx/stores/category_store.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
 import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/stores/user_manager_store.dart';
@@ -16,6 +18,7 @@ void main() async {
 void setupLocators() {
   GetIt.I.registerSingleton(PageStore());
   GetIt.I.registerSingleton(UserManagerStore());
+  GetIt.I.registerSingleton(CategoryStore());
 }
 
 Future<void> initializeParse() async {
