a46aeab79fcb975bd4d02a8973357a665eedc983
gustavo <gustavo@MacBook-Pro.local>
2021-07-15 21:30:41 -0300

    added favorite ad toggle button with toggle function

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:xlo_mobx/repositories/ibge_repository.dart';
 import 'package:xlo_mobx/screens/base/base_screen.dart';
 import 'package:xlo_mobx/screens/category/category_screen.dart';
 import 'package:xlo_mobx/stores/category_store.dart';
+import 'package:xlo_mobx/stores/favorite_store.dart';
 import 'package:xlo_mobx/stores/home_store.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
 import 'package:get_it/get_it.dart';
@@ -24,6 +25,7 @@ void setupLocators() {
   GetIt.I.registerSingleton(HomeStore());
   GetIt.I.registerSingleton(UserManagerStore());
   GetIt.I.registerSingleton(CategoryStore());
+  GetIt.I.registerSingleton(FavoriteStore());
 }
 
 Future<void> initializeParse() async {
--- lib/screens/ad/ad_screen.dart
+++ lib/screens/ad/ad_screen.dart
@@ -1,10 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/models/ad.dart';
 import 'package:xlo_mobx/screens/ad/components/description_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/location_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/user_panel.dart';
+import 'package:xlo_mobx/stores/favorite_store.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
 
 import 'components/bottom_bar.dart';
 import 'components/main_panel.dart';
@@ -14,12 +18,28 @@ class AdScreen extends StatelessWidget {
 
   final Ad ad;
 
+  final UserManagerStore userManagerStore = GetIt.I<UserManagerStore>();
+
+  final FavoriteStore favoriteStore = GetIt.I<FavoriteStore>();
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Anuncio"),
         centerTitle: true,
+        actions: [
+          if (ad.status == AdStatus.ACTIVE && userManagerStore.isLoggedIn)
+            Observer(builder: (_) {
+              return IconButton(
+                icon: Icon(favoriteStore.favoriteList
+                        .any((element) => element.id == ad.id)
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () => favoriteStore.toggleFavorite(ad),
+              );
+            }),
+        ],
       ),
       body: Stack(
         children: [
